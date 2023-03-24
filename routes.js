c55e3dc3f1005f54597a919b3bbc631914f1fe9b
Defarhan Nugraha <defarhannugraha1@gmail.com>
2023-03-24 11:34:10 +0700

    add more spesific routes config

--- routes.js
+++ routes.js
@@ -7,12 +7,33 @@ const routes = [
         },
     },
     {
+        method: '*',
+        path: '/',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tersebut';
+        },
+    },
+    {
         method: 'GET',
         path: '/about',
         handler: (request, h) => {
             return 'About Page';
         },
     },
+    {
+        method: '*',
+        path: '/about',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tersebut';
+        },
+    },
+    {
+        method: '*',
+        path: '/{any*}',
+        handler: (request, h) => {
+            return 'Halaman tidak ditemukan';
+        },
+    }
 ];
 
 module.exports = routes;
