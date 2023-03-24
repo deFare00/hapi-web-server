f451cc451fd123594d2db02750e0fc78dae79430
Defarhan Nugraha <defarhannugraha1@gmail.com>
2023-03-24 21:16:19 +0700

    add query parameters

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+// server.route({
+//     method: 'GET',
+//     path: '/users/{username?}',
+//     handler: (request, h) => {
+//         const {username = 'stranger'} = request.params;
+//         return `Hallo ${username}!`;
+//     },
+// });
+
 server.route({
     method: 'GET',
-    path: '/users/{username?}',
+    path: '/',
     handler: (request, h) => {
-        const {username = 'stranger'} = request.params;
-        return `Hallo ${username}!`;
+        const {name, location} = request.query;
+        return `Hello ${name} from ${location}!`;
     },
+
 });
